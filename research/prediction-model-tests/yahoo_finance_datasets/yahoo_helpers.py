233a9d987a1c4b44702fc1e6db9bcd3c6e431902
Karim <z5209523@ad.unsw.edu.au>
2021-02-19 23:03:39 +1100

    alterations made to save as actual .json object rather than string

---
+++ research/prediction-model-tests/yahoo_finance_datasets/yahoo_helpers.py
@@ -0,0 +1,18 @@
+import json
+import pandas as pd
+
+
+def json_to_dataframe(filename):
+    # need to put docstring
+    # need to put extensive error checking
+    # with open(filename, 'r') as infile:
+    #     json_string = infile.read()
+    
+    # json_data = json.loads(json_string)
+    # return pd.DataFrame.from_dict(json_data, orient='index')
+    with open(filename, 'r') as infile:
+        data = json.load(infile)
+        return pd.json_normalize(data['prices'])
+
+dataframe = json_to_dataframe('AMZN_20210219.json')
+print(dataframe.info)
